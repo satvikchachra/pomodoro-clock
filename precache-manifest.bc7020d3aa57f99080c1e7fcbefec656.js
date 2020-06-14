151dd848cbc449baffa9a662f085a5590aa58162
Satvik Chachra <50296407+satvikchachra@users.noreply.github.com>
2020-06-14 23:06:25 +0530

    Updates

---
+++ precache-manifest.bc7020d3aa57f99080c1e7fcbefec656.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d24686e206d2bacf6eec457953bd09e",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "26d9c99710b6acefdbb7",
+    "url": "/pomodoro-clock/static/css/main.fbe05fa3.chunk.css"
+  },
+  {
+    "revision": "7a56417dbca8edb1f8ba",
+    "url": "/pomodoro-clock/static/js/2.62c025bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.62c025bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26d9c99710b6acefdbb7",
+    "url": "/pomodoro-clock/static/js/main.0e62a6c0.chunk.js"
+  },
+  {
+    "revision": "a4dcdb46a55158e82e38",
+    "url": "/pomodoro-clock/static/js/runtime-main.6695a290.js"
+  }
+]);
